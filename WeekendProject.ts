e8104d9a230ae452b218433488f742e41c102fcf
sheilaj1832 <sheilajohnson1832@gmail.com>
2018-10-18 16:54:08 -0400

    Updates to the replaace a vowel weekend project

---
+++ WeekendProject.ts
@@ -0,0 +1,12 @@
+
+let copy = prompt("Enter a phrase to be magically transformed.")
+
+System.out.print("Enter string: ");
+String s = a.nextLine();
+String[] vals = {"a", "u", "o", "e", "i"};
+for(String val : vals)
+    s = s.replaceAll(val, "_");
+System.out.println(s);
+ 
+alert(s)
+    console.log(s)
